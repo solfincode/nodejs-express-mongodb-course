da5740dfdcd7284ffc2d66c92bffa29f5e611a4b
duo <hypersonolabs@gmail.com>
2022-05-17 13:34:08 +0900

    install cors and setup basic skeloton of express js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+/* Vanilla Node js 
+//import http module
+const http = require("http");
+
+//create server
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  console.log(url, method);
+});
+
+//setup port
+const PORT = 8000;
+
+//listening at server
+server.listen(PORT, () => console.log(`server is listening at ${PORT}`));
+*/
+
+const express = require("express");
+const PORT = 8000;
+const app = express();
+const cors = require("cors");
+
+//bodyparser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//listening server
+app.listen(PORT, () => console.log(`server is listening at ${PORT}`));
